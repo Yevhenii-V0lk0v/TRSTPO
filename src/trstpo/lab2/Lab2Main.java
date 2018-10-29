8d6064f3414436ed9f133cdd8e15fca41b16fef2
Yevhenii-V0lk0v <yevhenii.volkov@nure.ua>
2018-10-29 21:09:34 +0200

    Lab 2 start. Basic infrastructure for convenient development.

---
+++ src/trstpo/lab2/Lab2Main.java
@@ -0,0 +1,36 @@
+package trstpo.lab2;
+
+public class Lab2Main {
+
+	/**
+	 * Мэин для второй лабораторной
+	 *
+	 * @param args список номеров задач, которые нужно продемонстрировать
+	 */
+	public static void main(String[] args) {
+		for (String arg : args) {
+			launchModel(arg);
+		}
+	}
+
+	/**
+	 * Сюда вносить инициализацию вашего кода
+	 * Так будет проще демонстрировать код
+	 * Ну и хорошей идеей будет придумать какой-нибудь терминатор презентации
+	 *
+	 * @param arg строка-номер задачи
+	 */
+	private static void launchModel(String arg) {
+		switch (arg) {
+			case "2": {
+				break;
+			}
+			case "7": {
+				break;
+			}
+			default:
+		}
+	}
+
+
+}
